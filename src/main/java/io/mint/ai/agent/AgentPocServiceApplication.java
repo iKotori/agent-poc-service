a753bf5b10414e1f6862a8e3d5f6ed1a3b50196d
m1nt <mint@kotori.moe>
2026-04-16 16:07:56 +0800

    feat: init;

---
+++ src/main/java/io/mint/ai/agent/AgentPocServiceApplication.java
@@ -0,0 +1,13 @@
+package io.mint.ai.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentPocServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentPocServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/mint/ai/agent/AgentPocServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mint.ai.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentPocServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
